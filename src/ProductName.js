1ec5b94df8bd2acbeb66e1fe9abd3e2f108eddab
Allmighty95 <santiagogiraldo2018@outlook.es>
2024-08-29 15:46:06 -0500

    Initial commit

---
+++ src/ProductName.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ProductName(props) {
+  return (
+    <div>
+      <h3>{props.name}</h3>
+    </div>
+  );
+}
+export default ProductName;
---
+++ src/ProductPrice.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ProductPrice(props) {
+  return (
+    <div>
+      <p>${props.price}</p>
+    </div>
+  );
+}
+export default ProductPrice;
